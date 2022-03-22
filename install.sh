418bb073765c32c9be88177066db57da0515967d
eycjur <eycjur@gmail.com>
2022-03-22 16:44:06 +0900

    [add]gitconfig

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 
 set -CEeuxo pipefail
 
+DOT_FILES=(.zshrc .vimrc .gitconfig)
 DOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-for file in .zshrc .vimrc; do
+for file in ${DOT_FILES[@]}; do
   ln -sf "${DOT_DIR}/${file}" ~/"${file}"
 done
 
