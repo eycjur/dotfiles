3ea9bc1726776c6c7bdf37f17fe8bedfb398d82d
eycjur <eycjur@gmail.com>
2023-02-05 19:00:50 +0900

    [fix]zshでのディレクトリ移動の排除

--- .zsh/install_custom_commands.zsh
+++ .zsh/install_custom_commands.zsh
@@ -1,14 +1,11 @@
-# 作業はホームディレクトリに移動
-cd ~ > /dev/null
-
 # verコマンドがなければインストール
 if ! type ver > /dev/null 2>&1; then
-    git clone https://github.com/eycjur/cli-display-version.git
-    sudo ln -s $(pwd)/cli-display-version/ver /usr/local/bin/ver
+    git clone https://github.com/eycjur/cli-display-version.git ~/cli-display-version
+    sudo ln -s ~/cli-display-version/ver /usr/local/bin/ver
 fi
 
 # helコマンドがなければインストール
 if ! type hel > /dev/null 2>&1; then
-    git clone https://github.com/eycjur/cli-display-help.git
-    sudo ln -s $(pwd)/cli-display-help/hel /usr/local/bin/hel
+    git clone https://github.com/eycjur/cli-display-help.git ~/cli-display-help
+    sudo ln -s ~/cli-display-help/hel /usr/local/bin/hel
 fi
