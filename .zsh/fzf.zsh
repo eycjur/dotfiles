db40d1732390b2c86515c6a42266a4130eb2de6d
eycjur <eycjur@gmail.com>
2024-02-07 23:04:11 +0900

    feat: fzfコマンドの詳細か

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -7,6 +7,18 @@
 # H: コミットハッシュの選択
 # F: 編集済みファイルの選択
 # de: dockerコンテナに入る
+#
+# fzfコマンドのオプション
+#   -m: 複数選択、+m: 単一選択
+#   --no-sort: ソートしない
+#   --query: 検索文字列を指定
+#   --prompt: プロンプトを指定
+#   --preview: プレビューを表示
+# プレビューのオプション
+#   {}: 現在カーソルのある行の文字列
+#   {q}: 検索文字列
+#   {数字}: 現在カーソルのある行の文字列をスペースで区切った時のn番目(1始まり)の文字列
+
 
 # 過去に実行したコマンドを選択
 function select-history() {
@@ -37,20 +49,51 @@ function change-directory () {
 zle -N change-directory
 bindkey '^f' change-directory
 
-# TODO: fzfでキャンセルしたときに、後続のコマンドが実行されてしまうのを防ぐ
 # ブランチを切り替え
-alias swz='git switch $(git branch | fzf +m --query "$LBUFFER" --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g")'
+function switch-brach() {
+    local selected_branch=$(git branch | fzf +m --query "$LBUFFER" --prompt "GIT BRANCH>")
+    if [ -n "$selected_branch" ]; then
+        # (*| ) <branch> -> <branch>
+        git switch $(echo "$selected_branch" | sed -e "s/^\*\s*//g")
+    fi
+}
+alias swz=switch-brach
 
 # コミットハッシュを探す
-alias chz='git cherry-pick $(git log --all --oneline | fzf +m --query "$LBUFFER" --prompt "COMMIT HASH>" | cut -d" " -f1)'
+function select-chrry-pick() {
+    local selected_commit
+    selected_commit=$(git log --all --oneline | fzf +m --query "$LBUFFER" --prompt "COMMIT HASH>")
+    if [ -n "$selected_commit" ]; then
+        # <hash> <message> -> <hash>
+        git cherry-pick $(echo "$selected_commit"  | cut -d " " -f 1)
+    fi
+}
+alias chz=select-chrry-pick
 
 # 編集されたファイルを選択
-alias addz='git add $(unbuffer git status --short |
-        fzf --ansi --multi --query "$LBUFFER" --prompt "EDITED FILE>" --preview="
-            echo {} |
-            cut -c 3-|
-            xargs git diff --color
-        " | awk '\''{print $2}'\'')'
+function select-git-add-with-preview() {
+    local selected_files
+    selected_files=$(git status -uall --short |
+        fzf --ansi --multi --query "$LBUFFER" --prompt "EDITED FILE>" --preview='
+            if [[ {} =~ "^\?\?" ]]; then
+                cat {2};
+            else
+                git diff --color {2};
+            fi
+        ' | awk '{print $2}')
+    if [ -n "$selected_files" ]; then
+        git add $selected_files
+    fi
+}
+alias addz=select-git-add-with-preview
 
 # dockerコンテナに入る
-alias de='docker exec -it $(docker ps | fzf +m --query "$LBUFFER" --prompt "SELECT CONTAINER>" | cut -d " " -f 1) /bin/bash'
+select-docker-exec() {
+    local container_id
+    container_id=$(docker ps | fzf +m --query "$LBUFFER" --prompt "SELECT CONTAINER>")
+    if [ -n "$container_id" ]; then
+        # <container_id> <name> ... -> <container_id>
+        docker exec -it $(echo "$container_id" | cut -d " " -f 1) /bin/bash
+    fi
+}
+alias de=select-docker-exec
