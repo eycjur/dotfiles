c67cbf72e7d853dc3a54bd571ea858ced1307164
eycjur <eycjur@gmail.com>
2024-02-17 20:15:49 +0900

    feat: nvmを削除

--- .zsh/mac.zsh
+++ .zsh/mac.zsh
@@ -8,6 +8,4 @@ set_alias_if_success "awk" "gawk"
 set_alias_if_success "date" "gdate"
 set_alias_if_success "grep" "ggrep"
 
-run_if_exists $(brew --prefix nvm)/nvm.sh
-run_if_exists $(brew --prefix nvm)/etc/bash_completion.d/nvm
 run_if_exists "${HOME}/.iterm2_shell_integration.zsh"
